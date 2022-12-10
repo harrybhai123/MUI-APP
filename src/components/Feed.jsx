7b9512c7b72e36b96dd0cd51b9536b7a5ecc70ea
harrybhai123 <hariomtomar25832@gmail.com>
2022-12-10 23:28:15 +0530

    added Feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,58 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { Favorite, FavoriteBorder, MoreVert, Share } from "@mui/icons-material";
 
 const Feed = () => {
   return (
-    <Box bgcolor="pink" flex={4} p={2}>
-      Feed
+    <Box flex={4} p={2}>
+      <Card>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVert />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          component="img"
+          height="25%"
+          image="https://images.unsplash.com/photo-1571607388263-1044f9ea01dd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFtYm9yZ2hpbml8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to
+            cook together with your guests. Add 1 cup of frozen peas along with
+            the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+          </IconButton>
+          <IconButton aria-label="share">
+            <Share />
+          </IconButton>
+        </CardActions>
+      </Card>
     </Box>
   );
 };
